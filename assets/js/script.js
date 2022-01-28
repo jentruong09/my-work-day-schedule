e0f8ed72c1be23fc3534c93a51b4c577c2408d39
Jen Truong <jennifertruong09@gmail.com>
2022-01-27 21:07:44 -0500

    moved and deleted some tags in the html and started some code for the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
-
-
-
-
 // To add today's date to the header
 var today = moment().format('dddd, MMMM Do');
 $("#currentDay").text(today);
+
+
+
+var color = document.querySelectorAll('.description col-10')
+
+
+var currentHour = moment().hours()
+var plannerTime = document.querySelector('.time-block') // This is not a number, figure out what to do!
+console.log(currentHour)
+
+
+// Formating so that the current time is in red, future in green, and past in grey
+function setTimeColor() {
+    if (currentHour === plannerTime) {
+        color.attr('class', 'present')
+    }
+}
+
