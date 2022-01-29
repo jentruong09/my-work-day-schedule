553b5c2643f48dcd446fe0f662f7988e50b08afb
Jen Truong <jennifertruong09@gmail.com>
2022-01-29 11:07:13 -0500

    added the click, setItem, and getItem for the first 2 hours and added ids to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,90 +28,57 @@ $("textarea").each(function() {
 
 
 //Defining variable for save buttons
-var saveBtn = document.querySelectorAll(".saveBtn btn btn-primary col-1")
-var texts = document.querySelectorAll("textarea")
-var nineAM = document.querySelector("#9am")
+//var saveBtn = document.querySelectorAll(".saveBtn btn btn-primary col-1")
 
-function saveAllText(event) {
-    event.preventDefault();
-    localStorage.setItem("nineAM", $(nineAM.textContent()));
-}
+// var nineAmBox = document.getElementById("#nine-am")
+// var nineAmBox = document.getElementById("#nine-am")
 
 
 
+// $("button").on("click", function(){
+//     localStorage.setItem("nine", nineAmBox.val())
 
 
-
-
-$(".saveBtn btn btn-primary col-1").click(function(event){
-    saveAllText(event)
-})
-
-
-
-
-// saveBtn.addEventListener("click", function(event) {
-//     saveAllText(event);
-
-// });
-
-
-
-
-// //var amount = document.getElementsByClassName(".row time-block")
-// var amount = $(".row time-block")
-// console.log(amount.length)
-
-// for (var i = 0; i < amount.length; i++) {
-//     let scheduleTime = ($(amount[i]).children(".hour col-1"))//.attr("data"))
-//     console.log(scheduleTime)
-
-//     if (scheduleTime === military) {
-//         $(".description col-10").addClass("present")
-//     }
-
-// }
-
-
-
-
-
-
-
-
-
-//var scheduleTime = 
-
-// $("textarea").each(function() {
-//     var scheduleTime = parseInt($("id").attr(""));
-//     console.log(scheduleTime)
 // })
 
 
 
 
-
-// var color = document.querySelectorAll('.description col-10')
-
-// var currentHour = moment().hours()
-// console.log(currentHour)
+// $("#nine-am").children(".hour col-1").val(localStorage.getItem("nine"));
 
 
-// var scheduleTime = document.querySelector('.hour col-1')
-// console.log(scheduleTime)
-// var hour = scheduleTime.getAttribute('data-value')
-// console.log(hour)
-// // var scheduleTime = document.querySelectorAll('.time-number').textContent
-// // console.log(scheduleTime)
-
-// var plannerTime = document.querySelector('.time-number') // This is not a number, figure out what to do!
-// console.log(currentHour)
-// console.log(plannerTime)
 
 
-// function setTimeColor() {
-//     if (currentHour == scheduleTime) {
-//         color.attr('class', 'present')
-//     }
-// }
+
+// 9am timeslot
+$("button").on("click", function(){
+    var currentBox9 = parseInt($(this).parent().attr("data"));
+    var currentMessage9 = $("#nine-am").val();
+
+    localStorage.setItem("currentBox9", currentMessage9)
+    //console.log(currentBox9, currentMessage)
+})
+
+$("#nine-am").append(localStorage.getItem("currentBox9"));
+
+
+
+$("button").on("click", function(){
+    var currentBox10 = parseInt($(this).parent().attr("data"));
+    var currentMessage10 = $("#ten-am").val();
+
+    localStorage.setItem("currentBox10", currentMessage10)
+    
+})
+
+$("#ten-am").append(localStorage.getItem("currentBox10"));
+
+
+
+
+
+
+
+
+
 
